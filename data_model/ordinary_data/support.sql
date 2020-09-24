8b378b8a9217f1868f25caaf4e90af0ba5f42dcb
Lucie Nicolier <lucie@opengis.ch>
2020-09-24 14:43:41 +0200

    fix support and add support_type.sql

--- data_model/ordinary_data/support.sql
+++ data_model/ordinary_data/support.sql
@@ -6,9 +6,9 @@ CREATE TABLE siro_od.support
 (
     id uuid PRIMARY KEY,
     adress text,
-    fk_support_type_type integer,
+    fk_support_type integer,
     fk_owner text,
-    type_base integer,
+    fk_type_base integer,
     height numeric(8,3),
     height_free_under_signal numeric(8,3),
     date_install date,
@@ -18,5 +18,6 @@ CREATE TABLE siro_od.support
     picture text,
     geometry geometry(Point,2056) NOT NULL,
     CONSTRAINT fkey_vl_support_type FOREIGN KEY (fk_support_type) REFERENCES siro_vl.support_type (id) MATCH SIMPLE,
-    CONSTRAINT fkey_od_owner FOREIGN KEY (fk_owner) REFERENCES siro_od.owner (id) MATCH SIMPLE
+    CONSTRAINT fkey_od_owner FOREIGN KEY (fk_owner) REFERENCES siro_od.owner (id) MATCH SIMPLE,
+    CONSTRAINT fkey_type_base FOREIGN KEY (fk_type_base) REFERENCES siro_vl.type_base (id) MATCH SIMPLE
 );
---
+++ data_model/value_lists/support_type.sql
@@ -0,0 +1,18 @@
+
+
+CREATE TABLE siro_vl.support_type
+(
+  id serial PRIMARY KEY,
+  value_en text,
+  value_fr text,
+  value_de text,
+);
+
+INSERT INTO siro_vl (id, value_en, value_fr, value_de) VALUES (1, 'tubular', 'tubulaire', 'tubular');
+INSERT INTO siro_vl (id, value_en, value_fr, value_de) VALUES (2, 'triangulate', 'triangulé', 'triangulate');
+INSERT INTO siro_vl (id, value_en, value_fr, value_de) VALUES (3, 'gantry', 'portique', 'gantry');
+INSERT INTO siro_vl (id, value_en, value_fr, value_de) VALUES (4, 'lamppost', 'candélabre', 'lamppost');
+INSERT INTO siro_vl (id, value_en, value_fr, value_de) VALUES (5, 'jib', 'potence', 'jib');
+INSERT INTO siro_vl (id, value_en, value_fr, value_de) VALUES (6, 'facade', 'façade', 'facade');
+INSERT INTO siro_vl (id, value_en, value_fr, value_de) VALUES (7, 'wall', 'mur', 'wall');
+INSERT INTO siro_vl (id, value_en, value_fr, value_de) VALUES (7, 'other', 'autre', 'other');
