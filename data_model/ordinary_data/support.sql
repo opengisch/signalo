d1da357071b3d80ea23f6ae16343ab10c4b4a2d4
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-09-24 13:51:39 +0200

    fixes

--- data_model/ordinary_data/support.sql
+++ data_model/ordinary_data/support.sql
@@ -4,26 +4,19 @@
 
 CREATE TABLE siro_od.support
 (
-    id serial PRIMARY KEY,
-    adress text COLLATE pg_catalog."default",
-    adress_no abstime,
-    adress_additional text COLLATE pg_catalog."default",
-    type_support integer,
-    owner integer,
+    id uuid PRIMARY KEY,
+    adress text,
+    fk_support_type integer,
+    fk_owner text,
     type_base integer,
     height numeric(8,3),
     height_free_under_signal numeric(8,3),
     date_install date,
     date_last_stability_check date,
     state integer,
-    comment text COLLATE pg_catalog."default",
-    picture text COLLATE pg_catalog."default",
+    comment text,
+    picture text,
     geometry geometry(Point,2056) NOT NULL,
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-ALTER TABLE siro_od.support
-    OWNER to postgres;
+    CONSTRAINT fkey_vl_support_type FOREIGN KEY (fk_support_type) REFERENCES siro_vl.support_type (id) MATCH SIMPLE,
+    CONSTRAINT fkey_od_owner FOREIGN KEY (fk_owner) REFERENCES siro_od.owner (id) MATCH SIMPLE,
+);
