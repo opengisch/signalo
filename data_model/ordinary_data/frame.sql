ed36b0ed8d5ab8d587e5a84a0499680a62e8c0f9
Lucie Nicolier <lucie@opengis.ch>
2020-09-24 17:14:23 +0200

    add frame.sql

---
+++ data_model/ordinary_data/frame.sql
@@ -0,0 +1,17 @@
+-- Table: siro_od.frame
+
+-- DROP TABLE siro_od.frame;
+
+CREATE TABLE siro_od.frame
+(
+    id uuid PRIMARY KEY default uuid_generate_v1(),
+    fk_frame_type integer,
+    fk_fixing_type integer,
+    double-sided boolean,
+    state integer,
+    comment text,
+    picture text,
+    geometry geometry(Point,2056) NOT NULL,
+    CONSTRAINT fkey_vl_frame_type FOREIGN KEY (fk_frame_type) REFERENCES siro_vl.frame_type (id) MATCH SIMPLE,
+    CONSTRAINT fkey_vl_fixing_type FOREIGN KEY (fk_fixing_type) REFERENCES siro_vl.fixing_type (id) MATCH SIMPLE
+);
