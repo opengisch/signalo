bc9aa6d00dbf21de69ee249adfcab2d8d2bc6619
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-09-24 14:57:46 +0200

    complete statuS2

--- data_model/value_lists/status.sql
+++ data_model/value_lists/status.sql
@@ -9,4 +9,6 @@ CREATE TABLE siro_vl.status
   value_de text
 );
 
-INSERT INTO siro_vl.status (id, value_en, value_fr, value_de) VALUES (1, 'ok', 'fonctionnel', 'ok');
+INSERT INTO siro_vl.status (id, value_en, value_fr, value_de) VALUES (1, 'ok', 'en état', 'ok');
+INSERT INTO siro_vl.status (id, value_en, value_fr, value_de) VALUES (2, 'damaged', 'endommagé', 'damaged');
+INSERT INTO siro_vl.status (id, value_en, value_fr, value_de) VALUES (3, 'broken', 'détruit', 'broken');
