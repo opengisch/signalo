892d20c97fa9fd76d046885b86393c322336047a
Lucie Nicolier <lucie@opengis.ch>
2020-09-25 09:23:43 +0200

    rename pas de cadre with montage direct

--- data_model/value_lists/frame_type.sql
+++ data_model/value_lists/frame_type.sql
@@ -14,7 +14,7 @@ INSERT INTO siro_vl.support_type (id, value_en, value_fr, value_de) VALUES (2, '
 INSERT INTO siro_vl.support_type (id, value_en, value_fr, value_de) VALUES (3, 'to be determined', 'à déterminer', 'to be determined');
 
 
-INSERT INTO siro_vl.support_type (id, value_en, value_fr, value_de) VALUES (10, 'no frame', 'pas de cadre', 'no frame'); -- when the sign is directly attached to the support
+INSERT INTO siro_vl.support_type (id, value_en, value_fr, value_de) VALUES (10, 'direct assembly', 'montage direct', 'direct assembly'); -- when the sign is directly attached to the support
 INSERT INTO siro_vl.support_type (id, value_en, value_fr, value_de) VALUES (11, 'weld', 'soudé', 'weld');
 INSERT INTO siro_vl.support_type (id, value_en, value_fr, value_de) VALUES (12, 'fit', 'emboîté', 'fit');
 INSERT INTO siro_vl.support_type (id, value_en, value_fr, value_de) VALUES (13, 'with runner', 'avec glissières', 'with runner');
