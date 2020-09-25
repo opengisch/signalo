b99f6e088fe37dbbeaf4511465e373fc74edc66d
Lucie Nicolier <50236352+lucienicolier@users.noreply.github.com>
2020-09-25 10:40:14 +0200

    change the constraint from fixing_type to frame_fixing_type

--- data_model/ordinary_data/frame.sql
+++ data_model/ordinary_data/frame.sql
@@ -14,5 +14,5 @@ CREATE TABLE siro_od.frame
     picture text,
     CONSTRAINT fkey_vl_frame_type FOREIGN KEY (fk_frame_type) REFERENCES siro_vl.frame_type (id) MATCH SIMPLE,
     CONSTRAINT fkey_vl_status FOREIGN KEY (fk_status) REFERENCES siro_vl.status (id) MATCH SIMPLE,
-    CONSTRAINT fkey_vl_fixing_type FOREIGN KEY (fk_fixing_type) REFERENCES siro_vl.fixing_type (id) MATCH SIMPLE
+    CONSTRAINT fkey_vl_frame_fixing_type FOREIGN KEY (fk_frame_fixing_type) REFERENCES siro_vl.frame_fixing_type (id) MATCH SIMPLE
 );
