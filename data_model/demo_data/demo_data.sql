6ced9ba4eb3eca7da69b79d7f99e306f3175ae57
Lucie Nicolier <lucie@opengis.ch>
2020-09-28 11:32:49 +0200

    add demo data

---
+++ data_model/demo_data/demo_data.sql
@@ -0,0 +1,38 @@
+-- Insert supports
+
+INSERT INTO siro_od.support(fk_support_type,fk_support_base_type,fk_status,geometry)
+VALUES (1,1,1,ST_GeomFromText('POINT(2538388.0 1152589.8)',2056));
+INSERT INTO siro_od.support(fk_support_type,fk_support_base_type,fk_status,geometry)
+VALUES (1,1,1,ST_GeomFromText('POINT(2537954.42 1152523.14)',2056));
+INSERT INTO siro_od.support(fk_support_type,fk_support_base_type,fk_status,geometry)
+VALUES (3,2,1,ST_GeomFromText('POINT(2538449.48 1152512.22)',2056));
+
+-- Insert signs
+
+INSERT INTO siro_od.sign(fk_sign_type,fk_official_signe,fk_parent,fk_durability,fk_status,azimut)
+VALUES (1,1,1,1,1,20);
+INSERT INTO siro_od.sign(fk_sign_type,fk_official_signe,fk_parent,fk_durability,fk_status,azimut)
+VALUES (1,4,3,1,1,20);
+INSERT INTO siro_od.sign(fk_sign_type,fk_official_signe,fk_parent,fk_durability,fk_status,azimut)
+VALUES (1,1,1,1,1,100);
+INSERT INTO siro_od.sign(fk_sign_type,fk_official_signe,fk_parent,fk_durability,fk_status,azimut)
+VALUES (1,1,1,1,1,100);
+INSERT INTO siro_od.sign(fk_sign_type,fk_official_signe,fk_parent,fk_durability,fk_status,azimut)
+VALUES (1,1,1,1,1,100);
+INSERT INTO siro_od.sign(fk_sign_type,fk_official_signe,fk_parent,fk_durability,fk_status,azimut)
+VALUES (1,1,1,1,1,100);
+
+-- Insert frame
+
+INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
+VALUES (1,1,t,1);
+INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
+VALUES (1,1,t,1);
+INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
+VALUES (1,1,t,1);
+INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
+VALUES (1,1,t,1);
+INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
+VALUES (1,1,t,1);
+INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
+VALUES (1,1,t,1);
