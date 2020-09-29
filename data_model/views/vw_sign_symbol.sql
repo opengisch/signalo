d0e0a0aa302f8b576ab5dad971fae33696ac9dcb
Lucie Nicolier <lucie@opengis.ch>
2020-09-29 10:33:17 +0200

    add order by rank

--- data_model/views/vw_sign_symbol.sql
+++ data_model/views/vw_sign_symbol.sql
@@ -3,3 +3,4 @@ SELECT sign.*, support.geometry
 FROM siro_od.sign
 LEFT JOIN siro_od.frame ON frame.id = sign.fk_frame
 LEFT JOIN siro_od.support ON support.id = frame.fk_support
+ORDER BY frame.rank;
