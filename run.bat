9fc0cfaf3fbee9acaf65f9408d0ca66402081df6
Lucie Nicolier <lucie@opengis.ch>
2020-10-01 10:27:15 +0200

    modifi data_demo

--- run.bat
+++ run.bat
@@ -11,7 +11,7 @@ docker rm -f siro
 rem start the server
 rem -v mounts the source, so that changes to the datamodel don't require rebuild
 rem --rm delete the container when it stops (the data won't be persisted !)
-docker run -d -p 5432:5432 --name siro opengisch/siro
+docker run -d -p 5432:5432 -v "C:\Users\lunic\Documents\01_opengis\dev\signalisation_verticale:/src" --name siro opengisch/siro
 
 
-docker exec -e PGSERVICE=siro_build siro sh -c "init_db.sh build"
+docker exec -e PGSERVICE=siro_build siro sh -c "init_db.sh wait"
