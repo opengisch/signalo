3006ef2b8c2d172b7f5b7de9979e53fd3376a29b
Lucie Nicolier <50236352+lucienicolier@users.noreply.github.com>
2020-10-01 12:55:57 +0200

    fix demo data + draft for view

--- data_model/demo_data/demo_data.sql
+++ data_model/demo_data/demo_data.sql
@@ -3,38 +3,38 @@
 INSERT INTO siro_od.support(fk_support_type,fk_support_base_type,fk_status,geometry)
 VALUES (1,1,1,ST_GeomFromText('POINT(2538388.0 1152589.8)',2056));
 INSERT INTO siro_od.support(id,fk_support_type,fk_support_base_type,fk_status,geometry)
-VALUES ('70cbdbbe-0320-11eb-9a46-0242ac110002',1,1,1,ST_GeomFromText('POINT(2537954.42 1152523.14)',2056));
+VALUES ('00000000-0000-0000-0000-000000000001',1,1,1,ST_GeomFromText('POINT(2537954.42 1152523.14)',2056));
 INSERT INTO siro_od.support(fk_support_type,fk_support_base_type,fk_status,geometry)
 VALUES (3,2,1,ST_GeomFromText('POINT(2538449.48 1152512.22)',2056));
 
 -- Insert frame
 
-INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
-VALUES ('5869baae-0321-11eb-9a46-0242ac110002',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
-INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
-VALUES ('5869baae-0321-11eb-9a46-0242ac110003',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
-INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
-VALUES ('5869baae-0321-11eb-9a46-0242ac110004',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
-INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
-VALUES ('5869baae-0321-11eb-9a46-0242ac110005',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
-INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
-VALUES ('5869baae-0321-11eb-9a46-0242ac110006',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
-INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
-VALUES ('5869baae-0321-11eb-9a46-0242ac110007',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
+INSERT INTO siro_od.frame(id,rank,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('00000000-0000-0000-ffff-000000000002',1,1,1,true,1,'00000000-0000-0000-0000-000000000001');
+INSERT INTO siro_od.frame(id,rank,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('00000000-0000-0000-ffff-000000000003',2,1,1,true,1,'00000000-0000-0000-0000-000000000001');
+INSERT INTO siro_od.frame(id,rank,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('00000000-0000-0000-ffff-000000000004',3,1,1,true,1,'00000000-0000-0000-0000-000000000001');
+INSERT INTO siro_od.frame(id,rank,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('00000000-0000-0000-ffff-000000000005',4,1,1,true,1,'00000000-0000-0000-0000-000000000001');
+INSERT INTO siro_od.frame(id,rank,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('00000000-0000-0000-ffff-000000000006',5,1,1,true,1,'00000000-0000-0000-0000-000000000001');
+INSERT INTO siro_od.frame(id,rank,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('00000000-0000-0000-ffff-000000000007',6,1,1,true,1,'00000000-0000-0000-0000-000000000001');
 
 -- Insert signs
 
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
-VALUES (1,1,1,20,'5869baae-0321-11eb-9a46-0242ac110002');
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
-VALUES (1,3,1,19,'5869baae-0321-11eb-9a46-0242ac110003');
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
-VALUES (1,1,1,214,'5869baae-0321-11eb-9a46-0242ac110004');
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
-VALUES (1,1,1,210,'5869baae-0321-11eb-9a46-0242ac110005');
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
-VALUES (1,1,1,180,'5869baae-0321-11eb-9a46-0242ac110006');
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
-VALUES (1,1,1,180,'5869baae-0321-11eb-9a46-0242ac110007');
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
-VALUES (1,1,1,20,'5869baae-0321-11eb-9a46-0242ac110002');
+INSERT INTO siro_od.sign(id,rank,fk_sign_type,fk_official_sign,fk_durability,fk_status,azimut,fk_frame)
+VALUES ('00000000-0000-0000-eeee-000000000001',1,1,'1.01',1,1,20,'00000000-0000-0000-ffff-000000000002');
+INSERT INTO siro_od.sign(id,rank,fk_sign_type,fk_official_sign,fk_durability,fk_status,azimut,fk_frame)
+VALUES ('00000000-0000-0000-eeee-000000000002',1,1,'1.03',1,1,19,'00000000-0000-0000-ffff-000000000003');
+INSERT INTO siro_od.sign(id,rank,fk_sign_type,fk_official_sign,fk_durability,fk_status,azimut,fk_frame)
+VALUES ('00000000-0000-0000-eeee-000000000003',1,1,'1.25a',1,1,214,'00000000-0000-0000-ffff-000000000004');
+INSERT INTO siro_od.sign(id,rank,fk_sign_type,fk_official_sign,fk_durability,fk_status,azimut,fk_frame)
+VALUES ('00000000-0000-0000-eeee-000000000004',1,1,'1.30',1,1,210,'00000000-0000-0000-ffff-000000000005');
+INSERT INTO siro_od.sign(id,rank,fk_sign_type,fk_official_sign,fk_durability,fk_status,azimut,fk_frame)
+VALUES ('00000000-0000-0000-eeee-000000000005',1,1,'2.15.1',1,1,180,'00000000-0000-0000-ffff-000000000006');
+INSERT INTO siro_od.sign(id,rank,fk_sign_type,fk_official_sign,fk_durability,fk_status,azimut,fk_frame)
+VALUES ('00000000-0000-0000-eeee-000000000006',1,1,'2.16',1,1,180,'00000000-0000-0000-ffff-000000000007');
+INSERT INTO siro_od.sign(id,rank,fk_sign_type,fk_official_sign,fk_durability,fk_status,azimut,fk_frame)
+VALUES ('00000000-0000-0000-eeee-000000000007',2,1,'4.52',1,1,20,'00000000-0000-0000-ffff-000000000002');
---
+++ data_model/views/vw_sign_symbol.sql
@@ -0,0 +1,24 @@
+DROP VIEW IF EXISTS siro_od.vw_sign_symbol;
+
+CREATE OR REPLACE VIEW siro_od.vw_sign_symbol AS
+
+--SELECT
+--    sign.*,
+--    support.geometry
+--FROM siro_od.sign
+--LEFT JOIN siro_od.frame ON frame.id = sign.fk_frame
+--LEFT JOIN siro_od.support ON support.id = frame.fk_support
+
+SELECT
+    az_group.azimut,
+    sign.id,
+    frame.rank AS frame_rank,
+    sign.rank AS sign_rank,
+    sign.fk_official_sign
+FROM siro_od.sign
+LEFT JOIN siro_od.frame ON frame.id = sign.fk_frame
+LEFT JOIN siro_od.support ON support.id = frame.fk_support
+LEFT JOIN generate_series(-5,355,10) az_group (azimut)
+    ON sign.azimut >= az_group.azimut
+    AND sign.azimut < az_group.azimut + 10
+ORDER BY frame.rank, sign.rank;
