3a7e9fc5082890e80ed446453872a62aa83fdfa0
Lucie Nicolier <50236352+lucienicolier@users.noreply.github.com>
2020-10-01 10:29:45 +0200

    more complete demo data (#12)

--- data_model/demo_data/demo_data.sql
+++ data_model/demo_data/demo_data.sql
@@ -2,37 +2,39 @@
 
 INSERT INTO siro_od.support(fk_support_type,fk_support_base_type,fk_status,geometry)
 VALUES (1,1,1,ST_GeomFromText('POINT(2538388.0 1152589.8)',2056));
-INSERT INTO siro_od.support(fk_support_type,fk_support_base_type,fk_status,geometry)
-VALUES (1,1,1,ST_GeomFromText('POINT(2537954.42 1152523.14)',2056));
+INSERT INTO siro_od.support(id,fk_support_type,fk_support_base_type,fk_status,geometry)
+VALUES ('70cbdbbe-0320-11eb-9a46-0242ac110002',1,1,1,ST_GeomFromText('POINT(2537954.42 1152523.14)',2056));
 INSERT INTO siro_od.support(fk_support_type,fk_support_base_type,fk_status,geometry)
 VALUES (3,2,1,ST_GeomFromText('POINT(2538449.48 1152512.22)',2056));
 
--- Insert signs
-
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut)
-VALUES (1,1,1,20);
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut)
-VALUES (1,3,1,20);
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut)
-VALUES (1,1,1,100);
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut)
-VALUES (1,1,1,100);
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut)
-VALUES (1,1,1,100);
-INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut)
-VALUES (1,1,1,100);
-
 -- Insert frame
 
-INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
-VALUES (1,1,true,1);
-INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
-VALUES (1,1,true,1);
-INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
-VALUES (1,1,true,1);
-INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
-VALUES (1,1,true,1);
-INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
-VALUES (1,1,true,1);
-INSERT INTO siro_od.frame(fk_frame_type,fk_frame_fixing_type,double_sided,fk_status)
-VALUES (1,1,true,1);
+INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('5869baae-0321-11eb-9a46-0242ac110002',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
+INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('5869baae-0321-11eb-9a46-0242ac110003',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
+INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('5869baae-0321-11eb-9a46-0242ac110004',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
+INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('5869baae-0321-11eb-9a46-0242ac110005',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
+INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('5869baae-0321-11eb-9a46-0242ac110006',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
+INSERT INTO siro_od.frame(id,fk_frame_type,fk_frame_fixing_type,double_sided,fk_status,fk_support)
+VALUES ('5869baae-0321-11eb-9a46-0242ac110007',1,1,true,1,'70cbdbbe-0320-11eb-9a46-0242ac110002');
+
+-- Insert signs
+
+INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
+VALUES (1,1,1,20,'5869baae-0321-11eb-9a46-0242ac110002');
+INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
+VALUES (1,3,1,19,'5869baae-0321-11eb-9a46-0242ac110003');
+INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
+VALUES (1,1,1,214,'5869baae-0321-11eb-9a46-0242ac110004');
+INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
+VALUES (1,1,1,210,'5869baae-0321-11eb-9a46-0242ac110005');
+INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
+VALUES (1,1,1,180,'5869baae-0321-11eb-9a46-0242ac110006');
+INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
+VALUES (1,1,1,180,'5869baae-0321-11eb-9a46-0242ac110007');
+INSERT INTO siro_od.sign(fk_sign_type,fk_durability,fk_status,azimut,fk_frame)
+VALUES (1,1,1,20,'5869baae-0321-11eb-9a46-0242ac110002');
