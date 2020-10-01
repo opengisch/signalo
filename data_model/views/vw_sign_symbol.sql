55f46e6fb1429a8c5b33c2e966534fba3ff48919
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-10-01 12:59:47 +0200

    fix view

--- data_model/views/vw_sign_symbol.sql
+++ data_model/views/vw_sign_symbol.sql
@@ -14,11 +14,14 @@ SELECT
     sign.id,
     frame.rank AS frame_rank,
     sign.rank AS sign_rank,
-    sign.fk_official_sign
+    sign.fk_official_sign,
+    ROW_NUMBER () OVER (
+    PARTITION BY az_group.azimut
+    ) AS final_rank
 FROM siro_od.sign
 LEFT JOIN siro_od.frame ON frame.id = sign.fk_frame
 LEFT JOIN siro_od.support ON support.id = frame.fk_support
 LEFT JOIN generate_series(-5,355,10) az_group (azimut)
     ON sign.azimut >= az_group.azimut
     AND sign.azimut < az_group.azimut + 10
-ORDER BY frame.rank, sign.rank;
+ORDER BY azimut, final_rank;
