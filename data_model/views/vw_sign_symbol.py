dadbc84997daebef9eb6908bae604b8e4dddbb11
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-10-02 06:25:35 +0200

    follow up

--- data_model/views/vw_sign_symbol.py
+++ data_model/views/vw_sign_symbol.py
@@ -24,24 +24,35 @@ def vw_sign_symbol(srid: int, pg_service: str = None):
     cursor = conn.cursor()
 
     view_sql = """
-        CREATE OR REPLACE VIEW siro_od.vw_sign_symbol AS        
+        CREATE OR REPLACE VIEW siro_od.vw_sign_symbol AS
+        
+        WITH ordered_signs AS (      
+            SELECT
+                sign.id
+                , az_group.azimut_group
+                , {sign_columns}
+                , {vl_official_sign_columns}
+                , ROW_NUMBER () OVER (
+                    PARTITION BY azimut_group
+                    ) AS final_rank
+                , support.id AS support_id
+                , support.geometry::geometry(Point,%(SRID)s) AS support_geometry
+            FROM siro_od.sign
+            LEFT JOIN siro_od.frame ON frame.id = sign.fk_frame
+            LEFT JOIN siro_od.support ON support.id = frame.fk_support
+            LEFT JOIN siro_vl.official_sign ON official_sign.id = sign.fk_official_sign
+            LEFT JOIN generate_series(-5,355,10) az_group (azimut_group)
+                ON sign.azimut >= az_group.azimut_group
+                AND sign.azimut < az_group.azimut_group + 10
+            ORDER BY azimut_group, final_rank
+        )     
         SELECT
-            sign.id
-            , az_group.azimut_group
-            , {sign_columns}
-            , {vl_official_sign_columns}
-            , ROW_NUMBER () OVER (
-                PARTITION BY azimut_group
-                ) AS final_rank
-            , support.geometry::geometry(Point,%(SRID)s)
-        FROM siro_od.sign
-        LEFT JOIN siro_od.frame ON frame.id = sign.fk_frame
-        LEFT JOIN siro_od.support ON support.id = frame.fk_support
-        LEFT JOIN siro_vl.official_sign ON official_sign.id = sign.fk_official_sign
-        LEFT JOIN generate_series(-5,355,10) az_group (azimut_group)
-            ON sign.azimut >= azimut_group
-            AND sign.azimut < azimut_group + 10
-        ORDER BY azimut_group, final_rank;
+          ordered_signs.*,
+          SUM( vl_official_sign_img_height ) OVER rolling_window AS shift
+        FROM
+          ordered_signs
+          WINDOW rolling_window AS ( PARTITION BY support_id, azimut_group ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW )
+  ;
     """.format(
         sign_columns=select_columns(
             pg_cur=cursor, table_schema='siro_od', table_name='sign',
@@ -49,7 +60,7 @@ def vw_sign_symbol(srid: int, pg_service: str = None):
         ),
         vl_official_sign_columns=select_columns(
             pg_cur=cursor, table_schema='siro_vl', table_name='official_sign',
-            remove_pkey=False, indent=4, prefix='vl_official_sign'
+            remove_pkey=False, indent=4, prefix='vl_official_sign_'
         ),
     )
 
