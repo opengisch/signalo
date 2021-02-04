21139f56199b723efd29f0521b55984e90dea0a2
Lucie Nicolier <50236352+lucienicolier@users.noreply.github.com>
2021-02-04 15:56:05 +0100

    add editable sign for 2.30 (#32)

--- test/official_sign_images.sh
+++ test/official_sign_images.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../images/official
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../images/official/editable
 
 return_code=0
 
-LANGUAGES=(de fr it ro)
-for LANGUAGE in "${LANGUAGES[@]}"; do
-  IMAGES=$(psql -t -c "SELECT img_${LANGUAGE} FROM siro_vl.official_sign")
+_LANGUAGES=(de fr it ro)
+for _LANGUAGE in "${_LANGUAGES[@]}"; do
+  IMAGES=$(psql -t -c "SELECT img_${_LANGUAGE} FROM siro_vl.official_sign")
   for IMAGE in ${IMAGES}; do
     if [[ ! -f ${DIR}/${IMAGE} ]]; then
-      FID=$(psql -t -c "SELECT id FROM siro_vl.official_sign WHERE img_${LANG} = '${IMAGE}'")
-      echo "*** Image ${IMAGE} (id:${FID}) does not exist in ./images/official"
+      FID=$(psql -t -c "SELECT id FROM siro_vl.official_sign WHERE img_${_LANGUAGE} = '${IMAGE}'")
+      echo "*** Image ${IMAGE} (id:${FID}) does not exist in ./images/official/editable"
       return_code=1
     fi
   done
