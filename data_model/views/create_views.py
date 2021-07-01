abb647763dea6f52b4c559cecfd49d0dd2a4619b
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-07-01 10:46:31 +0200

    add edited supports view

--- data_model/views/create_views.py
+++ data_model/views/create_views.py
@@ -30,6 +30,8 @@ def create_views(srid: int,
     variables = {'SRID': srid}
 
     run_sql('data_model/views/drop_views.sql', pg_service, variables)
+
+    run_sql('data_model/views/vw_edited_support.sql', pg_service, variables)
     vw_sign_symbol(pg_service=pg_service, srid=srid)
 
 
