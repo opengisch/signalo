243085e0cee794d3e909e2f3a900ce046eddc65b
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-09-09 08:00:39 +0200

    remove hstore extension

--- data_model/changelogs/0001/0001_1.sql
+++ data_model/changelogs/0001/0001_1.sql
@@ -34,19 +34,6 @@ CREATE SCHEMA signalo_vl;
 
 ALTER SCHEMA signalo_vl OWNER TO postgres;
 
---
--- Name: hstore; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
-
-
---
--- Name: EXTENSION hstore; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION hstore IS 'data type for storing sets of (key, value) pairs';
-
 
 --
 -- Name: postgis; Type: EXTENSION; Schema: -; Owner: 
