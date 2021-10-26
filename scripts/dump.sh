27751002a60a7f8687bb25f900779e49c1cb3c03
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-10-26 10:32:50 +0200

    --no-owner in dump

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker exec signalo pg_dump --format=plain --exclude-schema=public --exclude-schema=app --column-inserts --file=dump-db.sql signalo
-docker exec signalo pg_dump --format=plain --schema=app --column-inserts --file=dump-app.sql signalo
+docker exec signalo pg_dump --format=plain --no-owner --exclude-schema=public --exclude-schema=app --column-inserts --file=dump-db.sql signalo
+docker exec signalo pg_dump --format=plain --no-owner --schema=app --column-inserts --file=dump-app.sql signalo
