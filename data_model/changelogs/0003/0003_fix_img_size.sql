b147d93930c4c4bac3e792ab094ab23cd73bd8f9
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-11-17 21:51:13 +0100

    Update 0003_fix_img_size.sql

--- data_model/changelogs/0003/0003_fix_img_size.sql
+++ data_model/changelogs/0003/0003_fix_img_size.sql
@@ -1,13 +1,14 @@
 
-UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.49-l';
-UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.49-r';
-UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.50.1-r';
-UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.50.1-l';
-UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.50.3-l';
-UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.50.3-r';
-UPDATE signalo_db.vl_official_sign SET img_width = 241 WHERE fk_official_sign = '4.49-l';
-UPDATE signalo_db.vl_official_sign SET img_width = 241 WHERE fk_official_sign = '4.49-r';
-UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE fk_official_sign = '4.50.1-r';
-UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE fk_official_sign = '4.50.1-l';
-UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE fk_official_sign = '4.50.3-l';
-UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE fk_official_sign = '4.50.3-r';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.49-l';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.49-r';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.50.1-r';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.50.1-l';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.50.3-l';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.50.3-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 241 WHERE id = '4.49-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 241 WHERE id = '4.49-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE id = '4.50.1-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE id = '4.50.1-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE id = '4.50.3-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE id = '4.50.3-r';
+
