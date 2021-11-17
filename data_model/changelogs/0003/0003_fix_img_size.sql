faff7c0dbaed73e5ba98c1bdde0610a641d1bd96
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-11-17 21:45:32 +0100

    fix changelog

--- data_model/changelogs/0003/0003_fix_img_size.sql
+++ data_model/changelogs/0003/0003_fix_img_size.sql
@@ -1,14 +1,13 @@
 
-UPDATE img_height SET 60 WHERE fk_official_sign = '4.49-l';
-UPDATE img_height SET 60 WHERE fk_official_sign = '4.49-r';
-UPDATE img_height SET 60 WHERE fk_official_sign = '4.50.1-r';
-UPDATE img_height SET 60 WHERE fk_official_sign = '4.50.1-l';
-UPDATE img_height SET 60 WHERE fk_official_sign = '4.50.3-l';
-UPDATE img_height SET 60 WHERE fk_official_sign = '4.50.3-r';
-UPDATE img_width SET 241 WHERE fk_official_sign = '4.49-l';
-UPDATE img_width SET 241 WHERE fk_official_sign = '4.49-r';
-UPDATE img_width SET 231 WHERE fk_official_sign = '4.50.1-r';
-UPDATE img_width SET 231 WHERE fk_official_sign = '4.50.1-l';
-UPDATE img_width SET 231 WHERE fk_official_sign = '4.50.3-l';
-UPDATE img_width SET 231 WHERE fk_official_sign = '4.50.3-r';
-
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.49-l';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.49-r';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.50.1-r';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.50.1-l';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.50.3-l';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE fk_official_sign = '4.50.3-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 241 WHERE fk_official_sign = '4.49-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 241 WHERE fk_official_sign = '4.49-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE fk_official_sign = '4.50.1-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE fk_official_sign = '4.50.1-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE fk_official_sign = '4.50.3-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE fk_official_sign = '4.50.3-r';
