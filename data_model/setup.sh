059f77933617cc607a9830ae7eed04b926ce4a23
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-11-18 09:49:24 +0100

    Update setup.sh

--- data_model/setup.sh
+++ data_model/setup.sh
@@ -74,7 +74,7 @@ fi
 
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0002/0002_1_sign-5.00.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0003/0003_fix_img_size.sql
-psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0003/0004_1.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0004/0004_fix_img_size_followup.sql
 
 
 ${DIR}/app/create_app.py --pg_service ${PGSERVICE} --srid=${SRID}
