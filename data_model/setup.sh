60fa1142ba90ee4336cdc1af5625fa834a9038e5
Lucie Nicolier <lucie@opengis.ch>
2021-11-18 08:58:56 +0100

    add changelogs in setup

--- data_model/setup.sh
+++ data_model/setup.sh
@@ -73,6 +73,8 @@ if [[ $demo_data == True ]]; then
 fi
 
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0002/0002_1_sign-5.00.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0003/0003_fix_img_size.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0003/0004_1.sql
 
 
 ${DIR}/app/create_app.py --pg_service ${PGSERVICE} --srid=${SRID}
