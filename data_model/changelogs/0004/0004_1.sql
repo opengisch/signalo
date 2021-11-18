c84d7f03b7deea407373081588a2b3285f262854
Lucie Nicolier <lucie@opengis.ch>
2021-11-18 08:09:51 +0100

    corr dimensions panneaux

--- data_model/changelogs/0004/0004_1.sql
+++ data_model/changelogs/0004/0004_1.sql
@@ -1,2 +1,14 @@
-UPDATE img_width SET 301 WHERE fk_official_sign = '4.50.4-l';
-UPDATE img_width SET 301 WHERE fk_official_sign = '4.50.4-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 301 WHERE id = '4.50.4-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 301 WHERE id = '4.50.4-r';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.49-l';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.49-r';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.50.1-r';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.50.1-l';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.50.3-l';
+UPDATE signalo_db.vl_official_sign SET img_height = 60 WHERE id = '4.50.3-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 241 WHERE id = '4.49-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 241 WHERE id = '4.49-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE id = '4.50.1-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE id = '4.50.1-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE id = '4.50.3-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 231 WHERE id = '4.50.3-r';
