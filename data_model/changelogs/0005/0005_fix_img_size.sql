81355769ef58e3a335fcc060f4c4ec4c40a19919
Lucie Nicolier <lucie@opengis.ch>
2021-12-17 14:34:07 +0100

    fix signs

---
+++ data_model/changelogs/0005/0005_fix_img_size.sql
@@ -0,0 +1,7 @@
+UPDATE signalo_db.vl_official_sign SET img_width = 72 WHERE id = '4.25b';
+UPDATE signalo_db.vl_official_sign SET img_width = 72 WHERE id = '4.25c';
+UPDATE signalo_db.vl_official_sign SET img_width = 72 WHERE id = '4.25d';
+UPDATE signalo_db.vl_official_sign SET img_width = 301 WHERE id = '4.50.4-l';
+UPDATE signalo_db.vl_official_sign SET img_width = 301 WHERE id = '4.50.4-r';
+UPDATE signalo_db.vl_official_sign SET img_width = 60 WHERE id = '4.51.1-a';
+UPDATE signalo_db.vl_official_sign SET img_width = 60 WHERE id = '4.51.1-b';
