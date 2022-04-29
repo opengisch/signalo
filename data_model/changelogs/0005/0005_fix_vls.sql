09b61c9994e0f43305da08b987b69f5184b7eaf8
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-04-29 14:31:00 +0200

    add sql

--- data_model/changelogs/0005/0005_fix_vls.sql
+++ data_model/changelogs/0005/0005_fix_vls.sql
@@ -1,48 +1,48 @@
 
 
-UPDATE TABLE signalo_db.vl_durability ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_durability ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_durability ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_durability ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_durability ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_durability ADD COLUMN description_de text;
 
-UPDATE TABLE signalo_db.vl_frame_fixing_type ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_frame_fixing_type ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_frame_fixing_type ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_frame_fixing_type ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_frame_fixing_type ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_frame_fixing_type ADD COLUMN description_de text;
 
-UPDATE TABLE signalo_db.vl_frame_type ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_frame_type ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_frame_type ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_frame_type ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_frame_type ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_frame_type ADD COLUMN description_de text;
 
-UPDATE TABLE signalo_db.vl_lighting ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_lighting ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_lighting ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_lighting ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_lighting ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_lighting ADD COLUMN description_de text;
 
-UPDATE TABLE signalo_db.vl_marker_type DROP COLUMN value_it;
-UPDATE TABLE signalo_db.vl_marker_type DROP COLUMN value_ro;
-UPDATE TABLE signalo_db.vl_marker_type ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_marker_type ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_marker_type ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_marker_type DROP COLUMN value_it;
+ALTER TABLE signalo_db.vl_marker_type DROP COLUMN value_ro;
+ALTER TABLE signalo_db.vl_marker_type ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_marker_type ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_marker_type ADD COLUMN description_de text;
 
-UPDATE TABLE signalo_db.vl_mirror_shape DROP COLUMN value_it;
-UPDATE TABLE signalo_db.vl_mirror_shape DROP COLUMN value_ro;
-UPDATE TABLE signalo_db.vl_mirror_shape ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_mirror_shape ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_mirror_shape ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_mirror_shape DROP COLUMN value_it;
+ALTER TABLE signalo_db.vl_mirror_shape DROP COLUMN value_ro;
+ALTER TABLE signalo_db.vl_mirror_shape ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_mirror_shape ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_mirror_shape ADD COLUMN description_de text;
 
-UPDATE TABLE signalo_db.vl_sign_type DROP COLUMN value_it;
-UPDATE TABLE signalo_db.vl_sign_type DROP COLUMN value_ro;
-UPDATE TABLE signalo_db.vl_sign_type ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_sign_type ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_sign_type ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_sign_type DROP COLUMN value_it;
+ALTER TABLE signalo_db.vl_sign_type DROP COLUMN value_ro;
+ALTER TABLE signalo_db.vl_sign_type ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_sign_type ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_sign_type ADD COLUMN description_de text;
 
-UPDATE TABLE signalo_db.vl_status ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_status ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_status ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_status ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_status ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_status ADD COLUMN description_de text;
 
-UPDATE TABLE signalo_db.vl_support_type ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_support_type ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_support_type ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_support_type ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_support_type ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_support_type ADD COLUMN description_de text;
 
-UPDATE TABLE signalo_db.vl_support_base_type ADD COLUMN description_en text;
-UPDATE TABLE signalo_db.vl_support_base_type ADD COLUMN description_fr text;
-UPDATE TABLE signalo_db.vl_support_base_type ADD COLUMN description_de text;
+ALTER TABLE signalo_db.vl_support_base_type ADD COLUMN description_en text;
+ALTER TABLE signalo_db.vl_support_base_type ADD COLUMN description_fr text;
+ALTER TABLE signalo_db.vl_support_base_type ADD COLUMN description_de text;
 
