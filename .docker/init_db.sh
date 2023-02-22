de7bd89cbc9d9817621a411c1ea7d156692bf931
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-02-22 09:38:57 +0100

    add docker script + small fixes

--- .docker/init_db.sh
+++ .docker/init_db.sh
@@ -5,8 +5,8 @@ DB_BASE_NAME=signalo
 set -e
 
 if [ ! "$#" == "0" ]; then
-  if [ ! "$1" == "wait" ] && [ ! "$1" == "build" ] && [ ! "$1" == "build_demo" ]; then
-    echo "arg must be one of : 'wait' 'build' 'build_demo'"
+  if [ ! "$1" == "wait" ] && [ ! "$1" == "build" ]; then
+    echo "arg must be one of : 'wait' 'build'"
     exit 1
   fi
 fi
@@ -29,7 +29,7 @@ recreate_db(){
 
 if [ "$1" == "wait" ]; then
   COUNT=0
-  printf "initializing DB…"
+  printf "waiting for Postgres…"
   until [[ -f ${PGDATA}/entrypoint-done-flag ]] && [[ $(( COUNT++ )) -ne 10 ]]; do
     printf " 🐘"
     sleep 3
@@ -49,22 +49,6 @@ if [ "$#" == "0" ] || [ "$1" == "build" ]; then
   ./data_model/setup.sh ${@:2}
 
   echo "Done ! Database ${PGSERVICE} can now be used."
-  echo '----------------------------------------'
-
-fi
-
-if [ "$1" == "build_demo" ]; then
-
-  recreate_db "${DB_BASE_NAME}_build_pum"
-  echo '----------------------------------------'
-  echo "Building database through pum migrations"
-
-  pum restore -p ${DB_BASE_NAME}_build_pum -x --exclude-schema public --exclude-schema topology -- ./test_data/qgep_demodata_1.0.0.dump
-  PGSERVICE=${DB_BASE_NAME}_build_pum psql -v ON_ERROR_STOP=on -f test_data/data_fixes.sql
-  pum baseline -p ${DB_BASE_NAME}_build_pum -t ${DB_BASE_NAME}_sys.pum_info -d ./delta/ -b 1.0.0
-  pum upgrade -p ${DB_BASE_NAME}_build_pum -t ${DB_BASE_NAME}_sys.pum_info -d ./delta/ -v int SRID 2056
-
-  echo "Done ! Database ${DB_BASE_NAME}_build_pum can now be used."
   echo '----------------------------------------'
 
 fi
