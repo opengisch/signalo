1b06353ae3487fd81721a1062f8591e12277f4a9
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-02-22 10:12:14 +0100

    ajout plaque de rue dans données démo

--- data_model/demo_data/dump.sh
+++ data_model/demo_data/dump.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-pg_dump --data-only -t signalo_db.sign --column-inserts | gsort | gsed '/^INSERT/!d' > data_model/demo_data/sign_content.sql
+TABLES=( sign azimut frame support )
+
+export PGSERVICE=pg_signalo
+
+for TABLE in "${TABLES[@]}"; do
+  pg_dump --data-only -t signalo_db.${TABLE} --column-inserts | gsort | gsed '/^INSERT/!d' > data_model/demo_data/${TABLE}_content.sql
+done
