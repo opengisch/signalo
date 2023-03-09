c8da60b43d11cd7b542df634b295f7fe5db3356d
Isabel Kiefer <isabelkiefer@MacBook-Air-von-Isabel.local>
2023-03-09 17:29:14 +0100

    delete english columns

---
+++ data_model/changelogs/0006/0006_delete_en.sql
@@ -0,0 +1,32 @@
+ALTER TABLE signalo_db.vl_coating DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_coating DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_durability DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_durability DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_frame_fixing_type DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_frame_fixing_type DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_frame_type DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_frame_type DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_lighting DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_lighting DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_marker_type DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_marker_type DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_mirror_shape DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_mirror_shape DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_sign_type DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_sign_type DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_status DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_status DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_support_base_type DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_support_base_type DROP COLUMN IF EXISTS description_en;
+
+ALTER TABLE signalo_db.vl_support_type DROP COLUMN IF EXISTS value_en;
+ALTER TABLE signalo_db.vl_support_type DROP COLUMN IF EXISTS description_en;
