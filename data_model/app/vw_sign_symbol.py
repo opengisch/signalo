e7de65d8c0b1bd49ad1ed89618dd017dff2f1c28
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-03-28 15:00:24 +0200

    mirror: image with and without redframe

--- data_model/app/vw_sign_symbol.py
+++ data_model/app/vw_sign_symbol.py
@@ -42,35 +42,39 @@ def vw_sign_symbol(srid: int, pg_service: str = None):
                   WHEN complex IS TRUE THEN 'complex.svg'
                   WHEN fk_sign_type = 11 THEN vl_official_sign.img_de 
                   WHEN fk_sign_type = 12 THEN 'marker.svg' 
-                  WHEN fk_sign_type = 13 THEN 'mirror.svg' 
+                  WHEN fk_sign_type = 13 AND mirror_red_frame IS TRUE THEN 'mirror.svg' 
+                  WHEN fk_sign_type = 13 AND mirror_red_frame IS FALSE THEN 'mirror-noframe.svg' 
                   WHEN fk_sign_type = 14 THEN 'street-plate.svg' 
                 END as _img_de
                 , CASE 
                   WHEN complex IS TRUE THEN 'complex.svg'
                   WHEN fk_sign_type = 11 THEN vl_official_sign.img_fr
                   WHEN fk_sign_type = 12 THEN 'marker.svg' 
-                  WHEN fk_sign_type = 13 THEN 'mirror.svg' 
+                  WHEN fk_sign_type = 13 AND mirror_red_frame IS TRUE THEN 'mirror.svg' 
+                  WHEN fk_sign_type = 13 AND mirror_red_frame IS FALSE THEN 'mirror-noframe.svg' 
                   WHEN fk_sign_type = 14 THEN 'street-plate.svg' 
                 END as _img_fr
                 , CASE 
                   WHEN complex IS TRUE THEN 'complex.svg'
                   WHEN fk_sign_type = 11 THEN vl_official_sign.img_it
                   WHEN fk_sign_type = 12 THEN 'marker.svg' 
-                  WHEN fk_sign_type = 13 THEN 'mirror.svg' 
+                  WHEN fk_sign_type = 13 AND mirror_red_frame IS TRUE THEN 'mirror.svg' 
+                  WHEN fk_sign_type = 13 AND mirror_red_frame IS FALSE THEN 'mirror-noframe.svg' 
                   WHEN fk_sign_type = 14 THEN 'street-plate.svg' 
                 END as _img_it
                 , CASE 
                   WHEN complex IS TRUE THEN 'complex.svg'
                   WHEN fk_sign_type = 11 THEN vl_official_sign.img_ro
                   WHEN fk_sign_type = 12 THEN 'marker.svg' 
-                  WHEN fk_sign_type = 13 THEN 'mirror.svg' 
+                  WHEN fk_sign_type = 13 AND mirror_red_frame IS TRUE THEN 'mirror.svg' 
+                  WHEN fk_sign_type = 13 AND mirror_red_frame IS FALSE THEN 'mirror-noframe.svg' 
                   WHEN fk_sign_type = 14 THEN 'street-plate.svg' 
                 END as _img_ro
                 , CASE 
                   WHEN complex IS TRUE THEN 106
                   WHEN fk_sign_type = 11 THEN vl_official_sign.img_height
                   WHEN fk_sign_type = 12 THEN 130
-                  WHEN fk_sign_type = 13 THEN 100
+                  WHEN fk_sign_type = 13 THEN 80
                   WHEN fk_sign_type = 14 THEN 50
                 END as _symbol_height
                 , CASE 
