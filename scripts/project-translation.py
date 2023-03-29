5f3dd0431ff60ceffe22d363cd338e9d76491b02
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-03-29 09:29:18 +0200

    push project translation (#87)

---
+++ scripts/project-translation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from qgis.core import QgsProject, QgsApplication
+
+app = QgsApplication([], True)
+app.setPrefixPath("/usr", True)
+app.initQgis()
+
+
+def print_message(message, tag, level):
+    print('{tag}: {message}'.format(tag=tag, message=message))
+
+
+app.messageLog().messageReceived.connect(print_message)
+
+project = QgsProject.instance()
+
+project.read('/usr/src/project/signalo.qgs')
+project.generateTsFile('en')
