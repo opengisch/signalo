1e63ff6040da6489310235b009b78aa25ec786b2
Isabel Kiefer <isabelkiefer@MacBook-Air-von-Isabel.local>
2023-04-03 14:45:59 +0200

    coating translations

---
+++ data_model/changelogs/0006/0006_coating.sql
@@ -0,0 +1,7 @@
+UPDATE signalo_db.vl_coating SET value_fr='type 1 qualité d''ingénieur' WHERE id = 11;
+UPDATE signalo_db.vl_coating SET value_fr='type 2 prismatique à haute intensité' WHERE id = 12;
+UPDATE signalo_db.vl_coating SET value_fr='type 3 qualité de diamant' WHERE id = 13;
+UPDATE signalo_db.vl_coating SET value_de='Typ 1 Ingenieursqualität' WHERE id = 11;
+UPDATE signalo_db.vl_coating SET value_de='Typ 2 hochintensiv prismatisch' WHERE id = 12;
+UPDATE signalo_db.vl_coating SET value_de='Typ 3 Diamantqualität' WHERE id = 13;
+UPDATE signalo_db.vl_coating SET value_de='Typ i beleuchtete Innenpaneele' WHERE id = 14;
