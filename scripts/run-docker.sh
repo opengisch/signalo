0aa49b3b08f8f8a718e4603a87cf8d38ad7a96b3
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-11-07 18:23:40 +0100

    add arm image + fix pip usage

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -3,6 +3,6 @@
 set -e
 
 docker build -f .docker/Dockerfile --tag opengisch/signalo .
-docker run -d -p 5432:5432 -v $(pwd):/src  --name signalo opengisch/signalo
+docker run -d -p 5433:5432 -v $(pwd):/src  --name signalo opengisch/signalo
 docker exec signalo init_db.sh wait
 docker exec signalo init_db.sh build -d
