4c285348c3480b66407317c4d344ab6187dd34c0
Isabel Kiefer <its.akifa@gmail.com>
2024-01-30 14:59:34 +0100

    update instead of insert

--- datamodel/changelogs/0100/0100_add_signs_img.sql
+++ datamodel/changelogs/0100/0100_add_signs_img.sql
@@ -1,7 +1,7 @@
-DELETE FROM signalo_db.vl_official_sign where id = '5.02';
-
-INSERT INTO signalo_db.vl_official_sign(
-	id, active, value_de, value_fr, value_it, value_ro, description_de, description_fr, description_it, description_ro, img_de, img_fr, img_it, img_ro, img_height, img_width, no_dynamic_inscription, default_inscription1, default_inscription2, default_inscription3, default_inscription4, directional_sign, img_de_right, img_fr_right, img_it_right, img_ro_right)
-	VALUES ('5.02', true, 'Anzeige von Entfernung und Richtung',
-    'Plaque indiquant la distance et la direction',
-    'Cartello indicante la distanza e la direzione', NULL, NULL, NULL, NULL, NULL, '502-l.svg', '502-l.svg', '502-l.svg', '502-l.svg', 80, 113, 0, '50 m', NULL, NULL, NULL, true, '502-r.svg', '502-r.svg', '502-r.svg', '502-r.svg');
+UPDATE signalo_db.vl_official_sign
+	
+    SET img_de='502-l.svg', img_fr='502-l.svg', img_it='502-l.svg', img_ro='502-l.svg',
+	directional_sign=true, 
+	img_de_right='502-r.svg', img_fr_right='502-r.svg', img_it_right='502-r.svg', img_ro_right='502-r.svg'
+	
+    WHERE id = '5.02';
