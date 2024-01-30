907a9c5f51966bfa8c2efd690d3ffdc2b5cc2cda
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-01-30 08:14:31 +0100

    improve compile script

--- scripts/project-translation-compile.sh
+++ scripts/project-translation-compile.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
-for f in /usr/src/project/*\_*.ts
+set -e
+
+PROJECT=$1
+
+if [[ -z ${PROJECT} ]]; then
+  echo "Script takes the QGIS project path or its directory as argument"
+  exit 1
+fi
+
+PATH=$(dirname ${PROJECT})
+
+echo $PATH
+
+if [[ ! -d ${PATH} ]]; then
+  echo "'${PATH}' directory does not exist"
+  exit 1
+fi
+
+FOUND=0
+for f in ${PATH}/*\_*.ts
 do
+  FOUND=1
 	lrelease $f ${f%.ts}.qm
 done
+
+if [[ ${FOUND} -eq 0 ]]; then
+  echo "No TS file found in '${PATH}'"
+fi
