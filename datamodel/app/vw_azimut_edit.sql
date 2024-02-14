126e3eacd9e7d1b64080867776faba66e9122e24
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-14 15:58:29 +0100

    add action to open form

--- datamodel/app/vw_azimut_edit.sql
+++ datamodel/app/vw_azimut_edit.sql
@@ -14,7 +14,7 @@ CREATE FUNCTION signalo_app.ft_azimut_insert() RETURNS trigger
     AS $$
     DECLARE
         support_id uuid := NULL;
-	BEGIN
+    BEGIN
         IF ST_NumPoints(NEW.geometry) != 2 THEN
             RAISE EXCEPTION 'The line should have only 2 vertices';
         END IF;
@@ -27,8 +27,8 @@ CREATE FUNCTION signalo_app.ft_azimut_insert() RETURNS trigger
         END IF;
         INSERT INTO signalo_db.azimut (fk_support, azimut) VALUES (support_id, degrees(ST_Azimuth(ST_StartPoint(NEW.geometry), ST_EndPoint(NEW.geometry))));
         RETURN NEW;
-	END;
-	$$;
+    END;
+    $$;
 
 CREATE TRIGGER azimut_insert
     INSTEAD OF INSERT ON signalo_app.vw_azimut_edit
@@ -41,7 +41,7 @@ CREATE TRIGGER azimut_insert
 CREATE FUNCTION signalo_app.ft_azimut_update() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
-	BEGIN
+    BEGIN
         IF NOT ST_Equals(ST_StartPoint(NEW.geometry), ST_StartPoint(OLD.geometry)) THEN
             RAISE EXCEPTION 'Start point should remain unchanged';
         END IF;
@@ -50,8 +50,8 @@ CREATE FUNCTION signalo_app.ft_azimut_update() RETURNS trigger
         END IF;
         UPDATE signalo_db.azimut SET azimut = degrees(ST_Azimuth(ST_StartPoint(NEW.geometry), ST_EndPoint(NEW.geometry))) WHERE id = NEW.id;
         RETURN NEW;
-	END;
-	$$;
+    END;
+    $$;
 
 CREATE TRIGGER azimut_update
     INSTEAD OF UPDATE ON signalo_app.vw_azimut_edit
