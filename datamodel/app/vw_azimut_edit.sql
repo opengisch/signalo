c42a2ac71286a7e7b04a879fd1300438bbbc280e
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-14 11:09:59 +0100

    create azimut from view + auto update map

--- datamodel/app/vw_azimut_edit.sql
+++ datamodel/app/vw_azimut_edit.sql
@@ -9,22 +9,48 @@ FROM signalo_db.azimut az
 INNER JOIN signalo_db.support su ON az.fk_support = su.id;
 
 
-CREATE FUNCTION signalo_app.ft_azimut_edit() RETURNS trigger
+CREATE FUNCTION signalo_app.ft_azimut_insert() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
+    DECLARE
+        support_id uuid := NULL;
 	BEGIN
-        IF ST_Equals(ST_StartPoint(NEW.geometry), ST_StartPoint(OLD.geometry)) THEN
-            RAISE WARNING 'start point should remain unchanged';
+        IF ST_NumPoints(NEW.geometry) != 2 THEN
+            RAISE EXCEPTION 'The line should have only 2 vertices';
         END IF;
-
-        UPDATE signalo_db.azimut SET azimut = degrees(ST_Azimuth(ST_StartPoint(NEW.geometry), ST_EndPoint(NEW.geometry))) WHERE id = NEW.id;
-
+        SELECT id FROM signalo_db.support s WHERE ST_Equals(s.geometry, ST_StartPoint(NEW.geometry)) INTO support_id;
+        IF support_id IS NULL THEN
+            RAISE EXCEPTION 'Could not find a support at the start of the line';
+        END IF;
+        INSERT INTO signalo_db.azimut (fk_support, azimut) VALUES (support_id, degrees(ST_Azimuth(ST_StartPoint(NEW.geometry), ST_EndPoint(NEW.geometry))));
         RETURN NEW;
 	END;
 	$$;
 
+CREATE TRIGGER azimut_insert
+    INSTEAD OF INSERT ON signalo_app.vw_azimut_edit
+    FOR EACH ROW
+    EXECUTE FUNCTION signalo_app.ft_azimut_insert();
+
+
+
+
+CREATE FUNCTION signalo_app.ft_azimut_update() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+	BEGIN
+        IF NOT ST_Equals(ST_StartPoint(NEW.geometry), ST_StartPoint(OLD.geometry)) THEN
+            RAISE EXCEPTION 'Start point should remain unchanged';
+        END IF;
+        IF ST_NumPoints(NEW.geometry) != 2 THEN
+            RAISE EXCEPTION 'The line should have only 2 vertices';
+        END IF;
+        UPDATE signalo_db.azimut SET azimut = degrees(ST_Azimuth(ST_StartPoint(NEW.geometry), ST_EndPoint(NEW.geometry))) WHERE id = NEW.id;
+        RETURN NEW;
+	END;
+	$$;
 
 CREATE TRIGGER azimut_update
     INSTEAD OF UPDATE ON signalo_app.vw_azimut_edit
     FOR EACH ROW
-    EXECUTE FUNCTION signalo_app.ft_azimut_edit();
+    EXECUTE FUNCTION signalo_app.ft_azimut_update();
