e39be38acf262bbcdf298577b64939704a8eb842
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-19 11:09:24 +0100

    fix

--- website/scripts/mkdocs_tx_commit.sh
+++ website/scripts/mkdocs_tx_commit.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [[ $(git diff --exit-code mkdocs.yml) ]]; then
   git config --global user.email "qgisninja@gmail.com"
   git config --global user.name "Geo Ninja"
 
   echo "detected changes in mkdocs.yml"
   if [[ ${GITHUB_EVENT} == "pull_request" ]]; then
+    echo "mkdocs is not updated from a PR"
     # on PR push to the same branch
-    gh pr checkout ${{ github.event.pull_request.number }}
-    git add mkdocs.yml
-    git commit -m "Update mkdocs.yml translation"
-    git push
+    # gh pr checkout ${{ github.event.pull_request.number }}
+    # git add mkdocs.yml
+    # git commit -m "Update mkdocs.yml translation"
+    # git push
   else
     # on push create a pull request
     BRANCH="update-mkdocs-tx-$RANDOM"
