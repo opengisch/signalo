b113431198878f0478677845d61c36fdc106b261
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-19 14:41:53 +0100

    fix change detection

--- website/scripts/mkdocs_tx_commit.sh
+++ website/scripts/mkdocs_tx_commit.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+pre-commit install
+pre-commit run --files mkdocs.yml || true
+
 if [[ $(git diff --exit-code mkdocs.yml) ]]; then
   git config --global user.email "github-actions[bot]@users.noreply.github.com"
   git config --global user.name "github-actions[bot]"
@@ -10,8 +13,6 @@ if [[ $(git diff --exit-code mkdocs.yml) ]]; then
   if [[ ${GITHUB_EVENT_NAME} == "pull_request" ]]; then
     # on PR push to the same branch
     gh pr checkout $(echo "${GITHUB_REF_NAME}" | cut -d/ -f1)
-    pre-commit install
-    pre-commit run --files mkdocs.yml || true
     git add mkdocs.yml
     git commit -m "Update mkdocs.yml translation" --no-verify
     git push
@@ -20,8 +21,6 @@ if [[ $(git diff --exit-code mkdocs.yml) ]]; then
     git checkout ${GITHUB_REF_NAME}
     BRANCH="update-mkdocs-tx-$RANDOM"
     git checkout -b ${BRANCH}
-    pre-commit install
-    pre-commit run --files mkdocs.yml || true
     git add mkdocs.yml
     git commit -m "Update mkdocs.yml translation" --no-verify
     git push -u origin $BRANCH
