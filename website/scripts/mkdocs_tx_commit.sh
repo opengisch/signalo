9a2d81dde371d6ac8cd5e66afcf4a8c8c65d12ef
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-19 14:33:31 +0100

    fix

--- website/scripts/mkdocs_tx_commit.sh
+++ website/scripts/mkdocs_tx_commit.sh
@@ -10,18 +10,20 @@ if [[ $(git diff --exit-code mkdocs.yml) ]]; then
   if [[ ${GITHUB_EVENT_NAME} == "pull_request" ]]; then
     # on PR push to the same branch
     gh pr checkout $(echo "${GITHUB_REF_NAME}" | cut -d/ -f1)
+    pre-commit install
+    pre-commit run --files mkdocs.yml || true
     git add mkdocs.yml
-    git commit -m "Update mkdocs.yml translation"
+    git commit -m "Update mkdocs.yml translation" --no-verify
     git push
   else
     # on push/workflow_dispatch create a pull request
     git checkout ${GITHUB_REF_NAME}
     BRANCH="update-mkdocs-tx-$RANDOM"
-    pre-commit install
     git checkout -b ${BRANCH}
+    pre-commit install
     pre-commit run --files mkdocs.yml || true
     git add mkdocs.yml
-    git commit -m "Update mkdocs.yml translation"
+    git commit -m "Update mkdocs.yml translation" --no-verify
     git push -u origin $BRANCH
     gh pr create -B ${GITHUB_REF_NAME} -H ${BRANCH} --title 'Update mkdocs translations' --body 'run from mkdocs_tx'
   fi
