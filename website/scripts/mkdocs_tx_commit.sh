94b9ab5bb129db0fe1123f6c61e9780c85bf7694
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-19 13:04:06 +0100

    fix

--- website/scripts/mkdocs_tx_commit.sh
+++ website/scripts/mkdocs_tx_commit.sh
@@ -20,8 +20,8 @@ if [[ $(git diff --exit-code mkdocs.yml) ]]; then
     git checkout -b ${BRANCH}
     git add mkdocs.yml
     git commit -m "Update mkdocs.yml translation"
-    echo "gh pr create -B ${GITHUB_REF} -H ${BRANCH} --title 'Update mkdocs translations' --body 'run from mkdocs_tx'"
-    gh pr create -B "${GITHUB_REF}" -H "${BRANCH}" --title 'Update mkdocs translations' --body 'run from mkdocs_tx'
+    echo "gh pr create -B ${GITHUB_REF_NAME} -H ${BRANCH} --title 'Update mkdocs translations' --body 'run from mkdocs_tx'"
+    gh pr create -B "${GITHUB_REF_NAME}" -H "${BRANCH}" --title 'Update mkdocs translations' --body 'run from mkdocs_tx'
   fi
 else
   echo "no change mkdocs.yml"
