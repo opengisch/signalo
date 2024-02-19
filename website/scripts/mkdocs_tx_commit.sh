b2cfe4d12bb467b36b3070cbdf2ca41b6787aa8e
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-19 09:47:58 +0100

    fix

---
+++ website/scripts/mkdocs_tx_commit.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [[ $(git diff --exit-code mkdocs.yml) ]]; then
+  echo "no change mkdocs.yml"
+else
+  echo "detected changes in mkdocs.yml"
+  if [[ ${{ github.event_name }} == "pull_request" ]]; then
+    # on PR push to the same branch
+    gh pr checkout ${{ github.event.pull_request.number }}
+    git add mkdocs.yml
+    git commit -m "Update mkdocs.yml translation"
+    git push
+  else
+    # on push create a pull request
+    git checkout ${{ github.head_ref }}
+    git checkout -b update-mkdocs-tx
+    git commit -m "Update mkdocs.yml translation"
+    gh pr create -B update-mkdocs-tx -H update-mkdocs-tx --title 'Update mkdocs translations'
+  fi
+fi
