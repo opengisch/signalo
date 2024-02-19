5678bbb197e001a17022c18a65546142c0dca61c
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-19 11:19:32 +0100

    fix

--- website/scripts/mkdocs_tx_commit.sh
+++ website/scripts/mkdocs_tx_commit.sh
@@ -21,8 +21,8 @@ if [[ $(git diff --exit-code mkdocs.yml) ]]; then
     git add mkdocs.yml
     git commit -m "Update mkdocs.yml translation"
     gh repo fork
-    git push ${BRANCH}
-    gh pr create -B "${GITHUB_REF}" -H "${BRANCH}" --title 'Update mkdocs translations' --body 'run from mkdocs_tx'
+    git push -u origin ${BRANCH}
+    gh pr create -B "${GITHUB_REF}" -H "upstream/${BRANCH}" --title 'Update mkdocs translations' --body 'run from mkdocs_tx'
   fi
 else
   echo "no change mkdocs.yml"
