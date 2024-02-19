9c059e000b0c15df6aab9e8d16209daea0db9c1c
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-19 11:05:54 +0100

    fix

--- website/scripts/mkdocs_tx_commit.sh
+++ website/scripts/mkdocs_tx_commit.sh
@@ -17,7 +17,8 @@ if [[ $(git diff --exit-code mkdocs.yml) ]]; then
     git checkout -b ${BRANCH}
     git add mkdocs.yml
     git commit -m "Update mkdocs.yml translation"
-    git push -u origin ${BRANCH}
+    gh repo fork
+    git push -u geoninja ${BRANCH}
     gh pr create -B "${GITHUB_REF}" -H "${BRANCH}" --title 'Update mkdocs translations' --body 'run from mkdocs_tx'
   fi
 else
