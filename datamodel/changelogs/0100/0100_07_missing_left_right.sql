c9d1d6b91dcb35a355c376c2c6623b4f18129fa4
Isabel Kiefer <its.akifa@gmail.com>
2024-02-27 12:21:26 +0100

    names of left-right 4.11-4.13 signs improved

---
+++ datamodel/changelogs/0100/0100_07_missing_left_right.sql
@@ -0,0 +1,29 @@
+ALTER TABLE signalo_db.vl_official_sign
+SET value_de = 'Fussgänger-Überführung (linksgehend)', 
+    value_fr = 'Passerelle pour piétons (vers la gauche)', 
+    value_it = 'Cavalcavia pedonale (a sinistra)' WHERE id = '4.13';
+
+ALTER TABLE signalo_db.vl_official_sign
+SET value_de = 'Fussgänger-Überführung (rechtsgehend)', 
+    value_fr = 'Passerelle pour piétons (vers la droite)', 
+    value_it = 'Cavalcavia pedonale (a destra)' WHERE id = '4.13a';
+
+ALTER TABLE signalo_db.vl_official_sign
+SET value_de = 'Fussgänger-Unterführung (linksgehend)', 
+    value_fr = 'Passage souterrain pour piétons (vers la gauche)', 
+    value_it = 'Sottopassaggio pedonale (a sinistra)' WHERE id = '4.12';
+
+ALTER TABLE signalo_db.vl_official_sign
+SET value_de = 'Fussgänger-Unterführung (rechtsgehend)', 
+    value_fr = 'Passage souterrain pour piétons (vers la droite)', 
+    value_it = 'Sottopassaggio pedonale (a destra)' WHERE id = '4.12a';
+
+ALTER TABLE signalo_db.vl_official_sign
+SET value_de = 'Standort eines Fussgängerstreifens (linksgehend)', 
+    value_fr = 'Emplacement d''un passage pour piétons (vers la gauche)', 
+    value_it = 'Ubicazione di un passaggio pedonale (a sinistra)' WHERE id = '4.11';
+
+ALTER TABLE signalo_db.vl_official_sign
+SET value_de = 'Standort eines Fussgängerstreifens (rechtsgehend)', 
+    value_fr = 'Emplacement d''un passage pour piétons (vers la droite)', 
+    value_it = 'Ubicazione di un passaggio pedonale (a destra)' WHERE id = '4.11a';
