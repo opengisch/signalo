f4c88474fb1c35264ffee6b366bd0de9eef990dd
Isabel Kiefer <its.akifa@gmail.com>
2024-02-27 12:21:26 +0100

    names of left-right 4.11-4.13 signs improved

--- datamodel/setup.sh
+++ datamodel/setup.sh
@@ -76,6 +76,7 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changel
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0100/0100_04_indexes.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0100/0100_05_change_official.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0100/0100_06_direction_user_signs.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0100/0100_07_missing_left_right.sql
 
 if [[ $demo_data == True ]]; then
   echo "*** inserting demo_data"
