156b3ae0a6331460e10e907d63a61fb2e9f25d56
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-11-05 12:24:24 +0100

    improve control/validation of changes

---
+++ datamodel/changelogs/0102/0101_02_control.sql
@@ -0,0 +1,24 @@
+ALTER TABLE signalo_db.support DROP COLUMN _edited;
+ALTER TABLE signalo_db.azimut DROP COLUMN _edited;
+ALTER TABLE signalo_db.frame DROP COLUMN _edited;
+ALTER TABLE signalo_db.sign DROP COLUMN _edited;
+
+ALTER TABLE signalo_db.support ADD COLUMN needs_validation boolean default false;
+ALTER TABLE signalo_db.azimut ADD COLUMN needs_validation boolean default false;
+ALTER TABLE signalo_db.frame ADD COLUMN needs_validation boolean default false;
+ALTER TABLE signalo_db.sign ADD COLUMN needs_validation boolean default false;
+
+ALTER TABLE signalo_db.support ADD COLUMN _last_modification_platform text default null;
+ALTER TABLE signalo_db.azimut ADD COLUMN _last_modification_platform text default null;
+ALTER TABLE signalo_db.frame ADD COLUMN _last_modification_platform text default null;
+ALTER TABLE signalo_db.sign ADD COLUMN _last_modification_platform text default null;
+
+ALTER TABLE signalo_db.support RENAME COLUMN _last_modified_date TO _last_modification_date;
+ALTER TABLE signalo_db.azimut RENAME COLUMN _last_modified_date TO _last_modification_date;
+ALTER TABLE signalo_db.frame RENAME COLUMN _last_modified_date TO _last_modification_date;
+ALTER TABLE signalo_db.sign RENAME COLUMN _last_modified_date TO _last_modification_date;
+
+ALTER TABLE signalo_db.support RENAME COLUMN _last_modified_user TO _last_modification_user;
+ALTER TABLE signalo_db.azimut RENAME COLUMN _last_modified_user TO _last_modification_user;
+ALTER TABLE signalo_db.frame RENAME COLUMN _last_modified_user TO _last_modification_user;
+ALTER TABLE signalo_db.sign RENAME COLUMN _last_modified_user TO _last_modification_user;
