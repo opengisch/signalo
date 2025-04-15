6d4339cfd96dd88ddef729921ff8c2c2ce06b489
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-04-15 12:17:53 +0200

    Add validation process (#265)
    * improve control/validation of changes
    
    * rename modified to modification
    
    * rename modified to modification
    
    * rename
    
    * default values ok
    
    * rename view to vw_validation
    
    * add action + trigger to validate a support + all children
    
    * fine tuning
    
    * add documentation
    
    * remove azimut tool from identifiable layers + fix display of sign images in form when locale is not supported
    
    * add page to index
    
    * fix images paths
    
    * fix default value
    
    * fix dep
    
    * typo
    
    * prefer ssl
    
    * fix view
    
    * fix script
    
    * deploy from control branch
    
    * fix dumps
    
    * push artifact
    
    * push comment
    
    * remove old file
    
    * add more info to validation view
    
    * remove default values
    
    * fix test
    
    * fix test

--- datamodel/app/vw_azimut_edit.sql
+++ datamodel/app/vw_azimut_edit.sql
@@ -4,6 +4,9 @@ CREATE OR REPLACE VIEW signalo_app.vw_azimut_edit
 AS
 SELECT
   az.id,
+  az._last_modification_date,
+  az._last_modification_user,
+  az.needs_validation,
   ST_MakeLine(su.geometry, ST_SetSRID(St_MakePoint(ST_X(su.geometry) + 10 * sin(radians(az.azimut)), ST_Y(su.geometry) + 10 *cos(radians(az.azimut))), 2056))::geometry(LineString, 2056) as geometry
 FROM signalo_db.azimut az
 INNER JOIN signalo_db.support su ON az.fk_support = su.id;
@@ -25,7 +28,19 @@ CREATE FUNCTION signalo_app.ft_azimut_insert() RETURNS trigger
                 RAISE EXCEPTION 'Could not create a support';
             END IF;
         END IF;
-        INSERT INTO signalo_db.azimut (fk_support, azimut) VALUES (support_id, degrees(ST_Azimuth(ST_StartPoint(NEW.geometry), ST_EndPoint(NEW.geometry))));
+        INSERT INTO signalo_db.azimut (
+                fk_support,
+                _last_modification_date,
+                _last_modification_user,
+                needs_validation,
+                azimut
+            ) VALUES (
+                support_id,
+                NEW._last_modification_date,
+                NEW._last_modification_user,
+                NEW.needs_validation,
+                degrees(ST_Azimuth(ST_StartPoint(NEW.geometry), ST_EndPoint(NEW.geometry)))
+            );
         RETURN NEW;
     END;
     $$;
@@ -48,7 +63,12 @@ CREATE FUNCTION signalo_app.ft_azimut_update() RETURNS trigger
         IF ST_NumPoints(NEW.geometry) != 2 THEN
             RAISE EXCEPTION 'The line should have only 2 vertices';
         END IF;
-        UPDATE signalo_db.azimut SET azimut = degrees(ST_Azimuth(ST_StartPoint(NEW.geometry), ST_EndPoint(NEW.geometry))) WHERE id = NEW.id;
+        UPDATE signalo_db.azimut SET
+            azimut = degrees(ST_Azimuth(ST_StartPoint(NEW.geometry), ST_EndPoint(NEW.geometry))),
+            _last_modification_date = NEW._last_modification_date,
+            _last_modification_user = NEW._last_modification_user,
+            needs_validation = NEW.needs_validation::boolean
+        WHERE id = NEW.id;
         RETURN NEW;
     END;
     $$;
