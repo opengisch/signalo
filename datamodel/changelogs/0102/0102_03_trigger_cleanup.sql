d70baaf9ea5a0e28f76b61f7ab07350ec3716cbf
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-05-19 09:19:16 +0200

    trigger cleanup

--- datamodel/changelogs/0102/0102_03_trigger_cleanup.sql
+++ datamodel/changelogs/0102/0102_03_trigger_cleanup.sql
@@ -1,5 +1,5 @@
 
 -- https://github.com/opengisch/signalo/blob/943368c9e39eedc1b38d89b6c238de7ba091fbeb/data_model/app/vw_sign_symbol.py#L138C32-L138C70
-DROP FUNCTION IF EXISTS signalo_app.ft_vw_sign_symbol_INSERT();
-DROP FUNCTION IF EXISTS signalo_app.ft_vw_sign_symbol_UPDATE();
-DROP FUNCTION IF EXISTS signalo_app.ft_vw_sign_symbol_DELETE();
+DROP FUNCTION TRIGGER IF EXISTS signalo_app.ft_vw_sign_symbol_INSERT();
+DROP FUNCTION TRIGGER IF EXISTS signalo_app.ft_vw_sign_symbol_UPDATE();
+DROP FUNCTION TRIGGER IF EXISTS signalo_app.ft_vw_sign_symbol_DELETE();
