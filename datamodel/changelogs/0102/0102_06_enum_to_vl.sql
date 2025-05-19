6003c6854f611a5f02d4ed80c70157a7aa172fbc
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-05-19 23:09:19 +0200

    default

--- datamodel/changelogs/0102/0102_06_enum_to_vl.sql
+++ datamodel/changelogs/0102/0102_06_enum_to_vl.sql
@@ -15,6 +15,7 @@ INSERT INTO signalo_db.vl_anchor (id, value_fr, value_de) VALUES ('center', 'cen
 INSERT INTO signalo_db.vl_anchor (id, value_fr, value_de) VALUES ('right', 'droite', 'rechts');
 
 ALTER TABLE signalo_db.frame ALTER COLUMN anchor SET DATA TYPE text USING lower(anchor::text);
+ALTER TABLE signalo_db.frame ALTER COLUMN anchor SET DEFAULT 'center';
 ALTER TABLE signalo_db.frame RENAME COLUMN anchor TO fk_anchor;
 ALTER TABLE signalo_db.frame ADD CONSTRAINT frame_fk_anchor_key FOREIGN KEY (fk_anchor) REFERENCES signalo_db.vl_anchor (id);
 
@@ -36,6 +37,7 @@ INSERT INTO signalo_db.vl_hanging_mode (id, value_fr, value_de) VALUES ('recto-v
 INSERT INTO signalo_db.vl_hanging_mode (id, value_fr, value_de) VALUES ('verso', 'verso', 'Verso');
 
 ALTER TABLE signalo_db.sign ALTER COLUMN hanging_mode SET DATA TYPE text USING lower(hanging_mode::text);
+ALTER TABLE signalo_db.sign ALTER COLUMN hanging_mode SET DEFAULT 'recto';
 ALTER TABLE signalo_db.sign RENAME COLUMN hanging_mode TO fk_hanging_mode;
 ALTER TABLE signalo_db.sign ADD CONSTRAINT frame_fk_hanging_mode_key FOREIGN KEY (fk_hanging_mode) REFERENCES signalo_db.vl_hanging_mode (id);
 
