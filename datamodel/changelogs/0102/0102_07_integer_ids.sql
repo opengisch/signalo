3a287734337507ef50c83625b6693d879d30fd2a
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-05-20 13:43:39 +0200

    switch to integer ids

---
+++ datamodel/changelogs/0102/0102_07_integer_ids.sql
@@ -0,0 +1,13 @@
+ALTER TABLE ONLY signalo_db.sign
+    DROP CONSTRAINT fkey_vl_marker_type;
+
+ALTER TABLE signalo_db.vl_marker_type
+    ALTER COLUMN id TYPE SMALLINT USING (replace(id, 'm-', '')::SMALLINT),
+    ALTER COLUMN id SET DEFAULT 1;
+
+
+ALTER TABLE signalo_db.sign
+    ALTER COLUMN fk_marker_type TYPE text USING (replace(fk_marker_type, 'm-', '')::SMALLINT);
+
+ALTER TABLE ONLY signalo_db.sign
+    ADD CONSTRAINT fkey_vl_marker_type FOREIGN KEY (fk_marker_type) REFERENCES signalo_db.vl_marker_type(id) MATCH FULL DEFERRABLE INITIALLY DEFERRED;
--- datamodel/setup.sh
+++ datamodel/setup.sh
@@ -87,6 +87,7 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changel
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0102/0102_04_comments.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0102/0102_05_offets_decimal.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0102/0102_06_enum_to_vl.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=${SRID} -f ${DIR}/changelogs/0102/0102_07_integer_ids.sql
 
 if [[ $demo_data == True ]]; then
   echo "*** inserting demo_data"
