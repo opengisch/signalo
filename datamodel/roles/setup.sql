4c7d8ce8987a23397af23d49556e77099707a577
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-09-19 16:25:14 +0200

    fine tune roles

--- datamodel/roles/setup.sql
+++ datamodel/roles/setup.sql
@@ -7,6 +7,18 @@ DECLARE
     viewer_role text := :'viewer_role';
     user_role text := :'user_role';
 BEGIN
+SELECT format('REVOKE CONNECT ON DATABASE %I FROM myuser;', datname)
+FROM pg_database
+WHERE datname NOT IN ('appdb');
+
+
+    FOR datname IN SELECT datname FROM pg_database
+        WHERE datname NOT IN ('signalo_testing', 'signalo_stable')
+    LOOP
+        EXECUTE format('REVOKE CONNECT ON DATABASE %I FROM %I', datname, viewer_role);
+    END LOOP;
+    EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', db_name, viewer_role);
+
     EXECUTE format('GRANT USAGE ON SCHEMA signalo_db TO %I', viewer_role);
     EXECUTE format('GRANT USAGE ON SCHEMA signalo_app TO %I', viewer_role);
     EXECUTE format('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA signalo_db TO %I', viewer_role);
