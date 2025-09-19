3e273cb20648cb297dbca76f15d6fa37dbc678ee
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-09-19 14:58:22 +0200

    variables for users

--- datamodel/roles/setup.sql
+++ datamodel/roles/setup.sql
@@ -2,29 +2,29 @@
 /* GRANT on schemas - once per database */
 ------------------------------------------
 
-/* Viewer */
-GRANT USAGE ON SCHEMA signalo_db  TO signalo_viewer;
-GRANT USAGE ON SCHEMA signalo_app  TO signalo_viewer;
+DO $$
+DECLARE
+    viewer_role text := :'viewer_role';
+    user_role text := :'user_role';
+BEGIN
+    EXECUTE format('GRANT USAGE ON SCHEMA signalo_db TO %I', viewer_role);
+    EXECUTE format('GRANT USAGE ON SCHEMA signalo_app TO %I', viewer_role);
+    EXECUTE format('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA signalo_db TO %I', viewer_role);
+    EXECUTE format('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA signalo_app TO %I', viewer_role);
+    EXECUTE format('GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA signalo_db TO %I', viewer_role);
+    EXECUTE format('GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA signalo_app TO %I', viewer_role);
+    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_db GRANT SELECT, REFERENCES, TRIGGER ON TABLES TO %I', viewer_role);
+    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_app GRANT SELECT, REFERENCES, TRIGGER ON TABLES TO %I', viewer_role);
 
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA signalo_db  TO signalo_viewer;
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA signalo_app TO signalo_viewer;
-
-GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA signalo_db  TO signalo_viewer;
-GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA signalo_app TO signalo_viewer;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_db  GRANT SELECT, REFERENCES, TRIGGER ON TABLES TO signalo_viewer;
-ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_app GRANT SELECT, REFERENCES, TRIGGER ON TABLES TO signalo_viewer;
-
-
-/* User */
-GRANT ALL ON SCHEMA signalo_db TO signalo_user;
-GRANT ALL ON ALL TABLES IN SCHEMA signalo_db TO signalo_user;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA signalo_db TO signalo_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_db GRANT ALL ON TABLES TO signalo_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_db GRANT ALL ON SEQUENCES TO signalo_user;
-
-GRANT ALL ON SCHEMA signalo_app TO signalo_user;
-GRANT ALL ON ALL TABLES IN SCHEMA signalo_app TO signalo_user;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA signalo_app TO signalo_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_app GRANT ALL ON TABLES TO signalo_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_app GRANT ALL ON SEQUENCES TO signalo_user;
+    EXECUTE format('GRANT ALL ON SCHEMA signalo_db TO %I', user_role);
+    EXECUTE format('GRANT ALL ON ALL TABLES IN SCHEMA signalo_db TO %I', user_role);
+    EXECUTE format('GRANT ALL ON ALL SEQUENCES IN SCHEMA signalo_db TO %I', user_role);
+    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_db GRANT ALL ON TABLES TO %I', user_role);
+    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_db GRANT ALL ON SEQUENCES TO %I', user_role);
+    EXECUTE format('GRANT ALL ON SCHEMA signalo_app TO %I', user_role);
+    EXECUTE format('GRANT ALL ON ALL TABLES IN SCHEMA signalo_app TO %I', user_role);
+    EXECUTE format('GRANT ALL ON ALL SEQUENCES IN SCHEMA signalo_app TO %I', user_role);
+    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_app GRANT ALL ON TABLES TO %I', user_role);
+    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA signalo_app GRANT ALL ON SEQUENCES TO %I', user_role);
+END
+$$;
