7afd5cf9f409d946a0740c92730e40ff4694d3f1
Isabel Kiefer <its.akifa@gmail.com>
2025-11-10 13:38:13 +0100

    more dynamic inscription corrections #341

--- datamodel/changelogs/1.2/0102_12_dynamic_signs.sql
+++ datamodel/changelogs/1.2/0102_12_dynamic_signs.sql
@@ -12,11 +12,26 @@ where value_fr LIKE '%3 ligne%';
 
 UPDATE signalo_db.vl_official_sign 
 SET value_de = 'Touristisch (3 Linien)'
-where id = '0.1-3'
+where id = '0.1-3';
 
 UPDATE signalo_db.vl_official_sign 
 SET no_dynamic_inscription = 1
-where id in ('0.3','1.10','1.11','2.16','2.17', '2.18', '2.19', '2.20','2.30', '2.47', '2.53', '2.59.1-NP', '2.59.2-NP'  );
+where id in ('0.2','0.3','1.10','1.11','2.16','2.17', '2.18', '2.19', '2.20','2.30', 
+'2.47', '2.53', '2.59.1-NP', '2.59.2-NP', '4.07a', '4.10', '4.22', '4.23', '4.46',
+'4.49', '4.50.1', '4.50.3', '4.50.4', '4.55', '4.56', '4.57', '4.58', '4.59', '4.59.1',
+'4.66', '4.71', '4.73','4.77/10-2', '5.01', '5.02', '5.03','5.15', '5.17','5.31-3');
+
+UPDATE signalo_db.vl_official_sign 
+SET no_dynamic_inscription = 2
+where id in ('4.36', '4.37', '4.40', '4.60', '4.62', '4.72', '4.90');
+
+UPDATE signalo_db.vl_official_sign 
+SET no_dynamic_inscription = 3
+where id in ('4.28', '4.30', '4.35', '4.39', '4.41', '4.42-2', '4.42', '4.53', '4.54', '4.61', '4.67', '4.69');
+
+UPDATE signalo_db.vl_official_sign 
+SET no_dynamic_inscription = 4
+where id in ('4.38', '4.64', '4.65', '4.68 ');
 
 UPDATE signalo_db.vl_official_sign
 SET value_de = 'Zonensignal Tempo-30-Zone',
@@ -50,3 +65,17 @@ SET value_de = 'Zonensignal Ende Parkieren',
     value_it = 'Segnale per fine della zona di parcheggio',
     value_fr = 'Signal de fin de zone de parc'
 where id = '2.59.2-P';
+
+UPDATE signalo_db.vl_official_sign
+SET value_de = 'Wegweiser für Umleitungen ohne Zielangabe',
+    value_it = 'Indicatore di direzione per deviazione senza menzione de luogo di destinazione',
+    default_inscription1 = NULL
+    where id = '4.34.1';
+
+UPDATE signalo_db.vl_official_sign
+SET default_inscription1 = NULL
+    where id = '4.51.1';
+
+UPDATE signalo_db.vl_official_sign
+SET default_inscription2 = NULL
+    where id = '5.00-1';
