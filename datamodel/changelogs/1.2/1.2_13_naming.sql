80cbf3c52b04bc34bb7b81c2a5692a58b568a25a
Isabel Kiefer <its.akifa@gmail.com>
2025-12-17 16:29:29 +0100

    solves #313

---
+++ datamodel/changelogs/1.2/1.2_13_naming.sql
@@ -0,0 +1,7 @@
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Piste cyclable et chemin pour piétons, avec partage de l''aire de circulation (exemple)'
+where id = '2.63';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Piste cyclable et chemin pour piétons sans partage de l''aire de circulation (exemple)'
+where id = '2.63.1';
