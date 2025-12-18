ddd482489e954cbda529c6a0e033eaa2ab9ca41b
Isabel Kiefer <its.akifa@gmail.com>
2025-12-18 09:44:54 +0100

    solves #361

--- datamodel/changelogs/1.2/1.2_13_naming.sql
+++ datamodel/changelogs/1.2/1.2_13_naming.sql
@@ -5,3 +5,53 @@ where id = '2.63';
 UPDATE signalo_db.vl_official_sign
 SET value_fr = 'Piste cyclable et chemin pour piétons sans partage de l''aire de circulation (exemple)'
 where id = '2.63.1';
+
+-- clean [[]]
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Passage à niveau sans barrières'
+where id = '1.16';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Passage de gibier'
+where id = '1.24';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Animaux'
+where id = '1.25';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Animaux (variante)'
+where id = '1.25a';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Vent latéral'
+where id = '1.29';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Autres dangers'
+where id = '1.30';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Bouchon'
+where id = '1.31';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Impasse'
+where id = '4.09';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Place de camping'
+where id = '4.79';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Poste de dépannage'
+where id = '4.83';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Poste d''essence'
+where id = '4.84';
+
+UPDATE signalo_db.vl_official_sign
+SET value_fr = 'Auberge de jeunesse'
+where id = '4.89';
+
