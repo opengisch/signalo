b63efbe5bf53e3bca70eda350b47d5e0a67d4e9a
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-12-27 11:57:07 +0100

    switch to python for testing images

---
+++ datamodel/changelogs/1.3/1.3_01_dimensions.sql
@@ -0,0 +1,5 @@
+UPDATE signalo_db.vl_official_sign
+SET img_width = 171, img_height = 116 WHERE id = '5.00-2';
+
+UPDATE signalo_db.vl_official_sign
+SET img_width = 171, img_height = 116 WHERE id = '5.00-3';
